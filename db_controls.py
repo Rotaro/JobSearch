c3aa1190281ea0d168c6c4f8d900f6118f04053e
Rotaro <-->
2015-08-16 14:51:48 +0300

    Looks pretty.

--- db_controls.py
+++ db_controls.py
@@ -2,7 +2,10 @@ import sqlite3
 import datetime
 
 class JobEntries:
-    """Class for creating and updating databases of job entries."""
+    """Class for creating and updating an sqlite database of job entries. The
+    database will store for each job entry a unique id, related search term, 
+    name of site containing job ad, job title, url to job ad and a column for  
+    possible additional  information. """
 
     db_filename = ""
     conn = None
@@ -20,6 +23,14 @@ class JobEntries:
                 c.execute('''CREATE TABLE JobEntries (site varchar(255), searchterm varchar(255), id varchar(255) PRIMARY KEY, title varchar(255), url varchar(1000), additional varchar(1000), date date);''')
 
     def set_new_entries(self, new_job_entries, site, search_term):
+        """ Stores new job entries into the data base. Each job entry will
+        contain a unique id, related search term, name of site containing job ad, 
+        job title, url to job ad and a column for possible additional  information.
+
+        new_job_entries - Dictionary of new job entries, {id : [title, url, additional]}
+        site            - Name of site job ad was found on.
+        search_term     - Search term which found the job ad.
+        """
         if(self.conn == None):
             self.conn = sqlite3.connect(self.db_filename)
         for_db = [(site, search_term, job_entry, new_job_entries[job_entry][0], new_job_entries[job_entry][1], new_job_entries[job_entry][2], datetime.date.today()) for job_entry in new_job_entries] 
@@ -44,19 +55,31 @@ class JobEntries:
 
     def generate_HTML_table(self, db_entries):
         """ Generates an HTML table of job entries from the database. """
-        html_start = """<html><head></head><body><table>"""
-        html_start = html_start + """<tr>
-        <td>Search term</td>
-        <td>Site</td>
-        <td>Job title</td>
-        <td>Description</td>
-        <td>Date</td>
-        <td>URL</td>
+        row_number = 0
+        html_start = """<!DOCTYPE HTML><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <script type="text/javascript">function hideRow(rownumber){document.getElementById(rownumber).style.display='none';}</script><link rel="stylesheet" href='jobsearch.css' /></head><body><table>"""
+        html_start = html_start + """<tr class="headers">
+        <th>Search term</th>
+        <th>Site</th>
+        <th>Job title</th>
+        <th>Description</th>
+        <th>Date</th>
+        <th>URL</th>
+        <td></th>
         </tr>"""
         for db_entry in db_entries.fetchall():
-            html_entry = """<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href="%s">Link</a></td></tr>""" % (
-                db_entry[1], db_entry[0], db_entry[3], db_entry[5], db_entry[6], db_entry[4])
+            html_entry = """<tr class="%s" origsite="%s" id="%d">
+            <td class="searchterm">%s</td>
+            <td class="site">%s</td>
+            <td class="jobtitle">%s</td>
+            <td class="description">%s</td>
+            <td class="date">%s</td>
+            <td class="url"><a href="%s">Link</a></td>
+            <td class="hidebutton"><input type="button" id="hidebutton" value="Hide" onclick='hideRow("%d");' />
+            </tr>""" % (
+                db_entry[1], db_entry[0], row_number, db_entry[1], db_entry[0], db_entry[3], db_entry[5], db_entry[6], db_entry[4], row_number)
             html_start = html_start + html_entry
+            row_number = row_number + 1;
         html_start = html_start + "</table></body></html>"
         return html_start
 
--- jobsearch.py
+++ jobsearch.py
@@ -1,12 +1,60 @@
-# -*- coding: iso-8859-1 -*-
-
 import parsers
 import db_controls
 import datetime
 import time
 import random
 import codecs
-import re
+
+class JobSearch:
+    """ Class for operating job searches. Searches for job ads from Indeed.fi, Duunitori.fi 
+    and Monster.fi. Results are stored in a sqlite database named during initiation. Entries
+    in the database can be output as an HTML table. """
+    db_name = ""
+    search_terms = []
+
+    def __init__(self, search_terms, db_name):
+        if (len(search_terms) == 0 or db_name == ""):
+            raise ValueError("Invalid arguments for JobSearch")
+        self.search_terms = search_terms
+        self.db_name = db_name
+
+    def start_search(self):
+        """ Starts search for job advertisements for the given search terms. 
+        HTML requests are randomly spread out by 3 to 5 seconds. """
+
+        random.seed(1222)
+        datab = db_controls.JobEntries(self.db_name)
+        datab.set_db()
+        for search_term in self.search_terms:
+            print("Searching for \"%s\"." % search_term)
+            time.sleep(random.randint(3,5))
+            monster_parser = parsers.MonsterParser()
+            indeed_parser = parsers.IndeedParser()
+            duunitori_parser = parsers.DuunitoriParser()
+            indeed_parser.parse_URL(parsers.URLGenerator.Indeed_URL(search_term))
+            monster_parser.parse_URL(parsers.URLGenerator.Monster_URL(search_term))
+            duunitori_parser.parse_URL(parsers.URLGenerator.Duunitori_URL(search_term))
+            datab.set_new_entries(indeed_parser.get_job_entries(), 'indeed', search_term)
+            datab.set_new_entries(monster_parser.get_job_entries(), 'monster', search_term)
+            datab.set_new_entries(duunitori_parser.get_job_entries(), 'duunitori', search_term)
+
+    def output_results_HTML(self, date, outputname):
+        """ Outputs job ads from the database as an HTML table. Job ads newer than the provided
+        date are included. If no date is provided, all job ads are taken from the database.
+        
+        date       - datetime.date object corresponding to a date. Job ads in the database which 
+                     are newer than this date are included in the output.
+        outputname - name of the file to store results in. 
+        """
+        
+        datab = db_controls.JobEntries(self.db_name)
+        datab.set_db()
+
+        print("Writing to %s from %s." % (outputname, self.db_name))
+        file = codecs.open(outputname, "w", encoding="utf-8")
+        file.write(datab.generate_HTML_table(datab.get_entries(date)))
+        file.close()
+
 
 search_terms = [
     'Analyytikko',
@@ -29,36 +77,11 @@ search_terms = [
     'Diplomi']
 
 
-random.seed(1222)
-
-new_db = db_controls.JobEntries("tmp.db")
-new_db.set_db()
-
-#for search_term in search_terms:
-#    print(search_term)
-#    time.sleep(random.randint(3,5))
-#    monster_parser = parsers.MonsterParser()
-#    indeed_parser = parsers.IndeedParser()
-#    duunitori_parser = parsers.DuunitoriParser()
-#    indeed_parser.parse_URL(parsers.URLGenerator.Indeed_URL(search_term))
-#    print('indeed')
-#    monster_parser.parse_URL(parsers.URLGenerator.Monster_URL(search_term))
-#    print('monster')
-#    duunitori_parser.parse_URL(parsers.URLGenerator.Duunitori_URL(search_term))
-#    print('duunitori')
-#    job_entries = monster_parser.get_job_entries()
-#    new_db.set_new_entries(indeed_parser.get_job_entries(), 'indeed', search_term)
-#    new_db.set_new_entries(monster_parser.get_job_entries(), 'monster', search_term)
-#    new_db.set_new_entries(duunitori_parser.get_job_entries(), 'duunitori', search_term)
 
 tod = datetime.date.today()
 yes = tod - datetime.timedelta(days=1)
 dayyes = tod - datetime.timedelta(days=2)
 
-
-file = codecs.open("test.html", "w", encoding="utf-8")
-file.write(new_db.generate_HTML_table(new_db.get_entries(yes)))
-file.close()
-
-
-
+js = JobSearch(search_terms, "tmp.db")
+js.start_search()
+js.output_results_HTML(yes, "test.html")
